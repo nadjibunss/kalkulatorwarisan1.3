eb8b3cd1c15672b4b9d00481ce56e8e953c9b521
nasky <nuridinawing121@gmail.com>
2025-08-24 12:03:07 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
