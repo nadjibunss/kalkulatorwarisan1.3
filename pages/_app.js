1f181b290cbf35a5d62e2ac99f212f339c7088c3
nasky <nuridinawing121@gmail.com>
2025-08-24 11:16:47 +0700

    Create _app.js

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import '../styles/globals.css';
+import { DataProvider } from '../context/DataContext';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <DataProvider>
+      <Component {...pageProps} />
+    </DataProvider>
+  );
+}
+
+export default MyApp;
