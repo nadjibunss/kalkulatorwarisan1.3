600e3b36b42ba53fda5581e22f85f6efd355d83c
nasky <nuridinawing121@gmail.com>
2025-08-24 13:41:18 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import '../styles/globals.css';
 import { DataProvider } from '../context/DataContext';
+import Footer from '../components/Footer';
 
 function MyApp({ Component, pageProps }) {
   return (
     <DataProvider>
-      <Component {...pageProps} />
+      <div className="flex flex-col min-h-screen bg-gray-100">
+        <main className="flex-grow">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
     </DataProvider>
   );
 }
