4adf50c5ceddf6fcfb62d93d779e375d58239574
nasky <nuridinawing121@gmail.com>
2025-08-24 13:46:06 +0700

    Create Footer.js

---
+++ components/Footer.js
@@ -0,0 +1,15 @@
+export default function Footer() {
+  return (
+    <footer className="text-center p-4 mt-8 text-gray-500 text-sm">
+      <p>Made with love for PABP smansa</p>
+      <a
+        href="https://wa.me/6283849080010"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-blue-500 hover:underline"
+      >
+        contact dev
+      </a>
+    </footer>
+  );
+}
